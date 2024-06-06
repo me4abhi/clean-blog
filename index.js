0874ffcde4cf334cb1379334035918d47718a5f6
Abhinav Saini <abhinavsaini669@gmail.com>
2024-06-06 20:09:40 +0530

    feat: :lipstick: added pages: about, contact

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
 
 app.listen(4000, () => {
   console.log('App listening on PORT:4000')
